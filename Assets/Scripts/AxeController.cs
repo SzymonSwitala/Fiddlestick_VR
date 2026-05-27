6cb3bac51ddc9d1015abaea2490ca521495fc7aa
Simon <>
2026-05-27 11:35:08 +0200

    update axe

--- Assets/Scripts/AxeController.cs
+++ Assets/Scripts/AxeController.cs
@@ -2,15 +2,51 @@ using UnityEngine;
 
 public class AxeController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("References")]
+    [SerializeField] private Rigidbody rb;
+
+    [Header("Damage")]
+    [SerializeField] private int damage = 1;
+    [SerializeField] private float minSwingVelocity = 1.5f;
+
+    [Header("Hit Settings")]
+    [SerializeField] private float hitCooldown = 0.15f;
+
+    private float lastHitTime;
+    private bool isSwinging;
+
+    public float CurrentVelocity { get; private set; }
+
+    private void Awake()
     {
-        
+        if (rb == null)
+            rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        CurrentVelocity = rb.linearVelocity.magnitude;
+
+        isSwinging = CurrentVelocity >= minSwingVelocity;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isSwinging)
+            return;
+
+        if (Time.time - lastHitTime < hitCooldown)
+            return;
+
+        TotemDestruction totem = other.GetComponent<TotemDestruction>();
+
+        if (totem == null)
+            return;
+
+        lastHitTime = Time.time;
+
+        Vector3 hitPoint = other.ClosestPoint(transform.position);
+
+        totem.TakeDamage(damage, hitPoint);
     }
 }
